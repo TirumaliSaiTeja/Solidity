7ea099991d3cafbfe6bff5cf0a7c60e9d667ff79
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-30 23:27:04 +0530

    added notes

--- Basic Solidity.sol
+++ Basic Solidity.sol
@@ -1,1 +1,11 @@
 // Intro to Solidity
+
+// Solidity is an object-oriented, high-level language for
+// implementing smart contracts. Smart contracts are programs which govern the behaviour
+// of accounts within the ethereum state.
+
+// Solidity is statically typed, support inheritance, libraries and
+// complex user-defined types among other featured
+
+// For writing smart contracts we can use remix ide 
+
